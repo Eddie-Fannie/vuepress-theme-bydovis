1cf37071dcba5762f4b750eacbfbcaaaa74b5143
Eddie-Fannie <1597271709@qq.com>
2020-05-19 20:03:16 +0800

    修改轮播

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="carousel-container">
+  <div>
       <el-carousel :interval="4000" type="card" height="200px">
-        <el-carousel-item v-for="item in 3" :key="item">
-          <h3 class="medium">{{ item }}</h3>
+        <el-carousel-item v-for="(item, index) in recommendData" :key="index">
+          <div class="article-list" :style="{backgroundImage: 'url(' + item.banner +')'}" @click="toDetail(item.link)">
+            <h3>{{item.title}}</h3>
+          </div>
+          
         </el-carousel-item>
       </el-carousel>
   </div>
@@ -15,8 +18,16 @@ export default {
 
     }
   },
+  props: {
+    recommendData: {
+      type: Array,
+      required: true
+    }
+  },
   methods: {
-
+    toDetail (payload) {
+      this.$router.push(payload)
+    }
   },
   components: {
 
@@ -26,18 +37,13 @@ export default {
 
 <style scoped>
   .el-carousel__item h3 {
-    color: #475669;
-    font-size: 14px;
-    opacity: 0.75;
+    color: #ffffff;
+    font-size: 24px;
     line-height: 200px;
     margin: 0;
   }
-  
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
+  .article-list {
+    background: no-repeat center;
+    background-size: cover;
   }
 </style>
--- components/Home.vue
+++ components/Home.vue
@@ -4,13 +4,12 @@
     aria-labelledby="main-title"
   >
     <header class="hero" :style="{backgroundImage: 'url('+data.heroBannerImage+')'}">
-      <div class="mess-container">
-          <img
+      <img
           v-if="data.heroImage"
           :src="$withBase(data.heroImage)"
           :alt="data.heroAlt || 'hero'"
         >
-
+      <div class="mess-container">
         <h1
           v-if="data.heroText !== null"
           id="main-title"
@@ -35,7 +34,7 @@
           />
         </p>
       </div>
-      <Carousel/>
+      <Carousel class="carousel-container" :recommendData = "data.recommendPost"/>
     </header>
 
     <div
@@ -101,38 +100,41 @@ export default {
     display flex
     justify-content flex-start
     align-items center
-    .mess-container 
-      width 30%
     img
       max-width: 100%
       max-height 280px
       display block
+      margin-left 5rem
       // margin-top 3rem
       // margin 3rem auto 1.5rem
-    h1
-      font-size 3rem
-    h1, .description, .action
-      margin 1.8rem auto
-    .description
-      max-width 35rem
-      font-size 1.125rem 
-      line-height 1.3
-      color lighten($bannerTextColor, 40%)
-    .action-button
-      display inline-block
-      font-size 1.2rem
-      color #fff
-      background-color $accentColor
-      padding 0.8rem 1.6rem
-      border-radius 4px
-      transition background-color .1s ease
-      box-sizing border-box
-      border-bottom 1px solid darken($accentColor, 10%)
-      &:hover
-        background-color lighten($accentColor, 10%)
+    .mess-container 
+      width 20%
+      h1
+        font-size 3rem
+      h1, .description, .action
+        margin 1.8rem auto
+      .description
+        max-width 35rem
+        font-size 1.125rem 
+        line-height 1.3
+        color lighten($bannerTextColor, 40%)
+      .action-button
+        display inline-block
+        font-size 1.2rem
+        color #fff
+        background-color $accentColor
+        padding 0.8rem 1.6rem
+        border-radius 4px
+        transition background-color .1s ease
+        box-sizing border-box
+        border-bottom 1px solid darken($accentColor, 10%)
+        &:hover
+          background-color lighten($accentColor, 10%)
+    .carousel-container
+      width 60%
   .features
     border-top 1px solid $borderColor
-    padding 1.2rem 0
+    padding 1.2rem 10rem
     margin-top 2.5rem
     display flex
     flex-wrap wrap
